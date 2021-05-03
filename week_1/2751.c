f81461a0c9f866388a5a9b043eaeb32fe83423f8
moong <moonki0623@naver.com>
2021-05-03 19:46:45 +0900

    2751.c to week1

---
+++ week_1/2751.c
@@ -0,0 +1,32 @@
+/*
+week 1 binary search
+http://acmicpc.net/problem/2751
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp (const void *, const void*);
+
+int main(){
+    int n, *arr;
+    scanf("%d", &n);
+
+    arr = (int*) malloc(n * sizeof(int));
+
+    for(int i = 0; i < n; i++){
+        scanf("%d", arr+i);
+    }
+
+    qsort(arr, n, sizeof(int), cmp);
+
+    for(int i=0;i<n;i++){
+        printf("%d\n", *(arr+i));
+    }
+
+    return 0;
+}
+
+int cmp (const void *a, const void *b){
+    return *(int*)a - *(int*)b;
+}
