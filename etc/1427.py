3c515661df64e42aa30d9f1caaaeabdfd8e03d6c
moong <moonki0623@naver.com>
2023-03-10 17:36:58 +0900

    1427

--- etc/1427.py
+++ etc/1427.py
@@ -2,6 +2,10 @@ num = list(input())
 arr = []
 for i in num:
     arr.append(int(i))
-arr.sort(reverse=True)
+# arr.sort(reverse=True)
+# for i in range(len(arr)):
+#     print(arr[i], end='')
+
+arr.sort()
 for i in range(len(arr)):
-    print(arr[i], end='')
+    print(arr[len(arr) - i-1], end='')
