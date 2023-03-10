4da02eaa7c06ba1f4b4aedad970ad6fe5ee0e773
moong <moonki0623@naver.com>
2023-03-10 17:35:38 +0900

    1009

---
+++ etc/1009.py
@@ -0,0 +1,21 @@
+num = int(input())
+for _ in range(num):
+    a, b = map(int, input().split())
+    a = a % 10
+
+    if a == 0:
+        print(10)
+    elif a in [1, 5, 6]:
+        print(a)
+    elif a in [4, 9]:
+        b = b % 2
+        if b == 0:
+            print(a*a % 10)
+        else:
+            print(a)
+    else:
+        b = b % 4
+        if b == 0:
+            print(a**4 % 10)
+        else:
+            print(a**b % 10)
