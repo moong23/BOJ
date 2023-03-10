91c03dafcf3881539f20a6e01ad10a059b65fe11
moong <moonki0623@naver.com>
2023-03-10 17:37:03 +0900

    1546

---
+++ etc/1546.py
@@ -0,0 +1,18 @@
+# num = int(input())
+
+# d = []
+
+# for i in map(int, input().split()):
+#     d.append(i)
+
+# print(d)
+
+num = int(input())
+
+d = []
+
+max = -1
+for i in input().split():
+    d.append(int(i))
+
+print(d)
