25b82bbc4107707b089a125c985ee12d76055c97
moong <moonki0623@naver.com>
2023-03-10 17:38:15 +0900

    2439

--- etc/2439.py
+++ etc/2439.py
@@ -2,3 +2,17 @@ a = int(input())
 for b in range(1, 1+a):
     c = a-b
     print(' '*c + '*'*b)
+
+
+# num = int(input())
+
+# for x in range(num):
+#     for _ in range(num-1, x, -1):
+#         print(' ', end='')
+#     for _ in range(x+1):
+#         print('*', end='')
+#     print('')
+
+# for x in range(1, num+1):  # x = [1,2,3,4,5]
+#     print(' '*(num-x), end='')
+#     print('*'*x, end='')
