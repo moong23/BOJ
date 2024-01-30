f8b020798165c3059e21aebf4267627b10cc1fea
Moon Ki, Kim <moonki0623@naver.com>
2024-01-30 17:38:54 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 165 ms, Memory: 30,224 KB -BaekjoonHub

---
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -0,0 +1,78 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+    public static StringBuilder sb = new StringBuilder();
+	public static int[][] ladder = new int[100][100];
+	public static int target;
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for (int testcse = 1; testcse <= 10; testcse++) {
+			int tc = Integer.parseInt(br.readLine());
+			for (int i = 0; i < 100; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < 100; j++) {
+					ladder[i][j] = Integer.parseInt(st.nextToken());
+					if (i == 99 && ladder[i][j] == 2)
+						target = j;
+				}
+			}
+
+			for (int i = 99; i >= 0; i--) {
+				if (target >= 1 && ladder[i][target - 1] == 1) { // 왼쪽 사다리
+					while (ladder[i][target - 1] == 1) {
+						target--;
+						if (target == 0)
+							break;
+					}
+				} else if (target < 99 && ladder[i][target + 1] == 1) {// 오른쪽 사다리
+					while (ladder[i][target + 1] == 1) {
+						target++;
+						if (target == 99)
+							break;
+					}
+				}
+
+			}
+
+			sb.append("#").append(tc).append(" ").append(target).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
