3f7da294d3405d21626b3aed64be6d182960c3be
Moon Ki, Kim <moonki0623@naver.com>
2024-04-15 14:51:23 +0900

    [Silver IV] Title: 스택, Time: 92 ms, Memory: 34176 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,33 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N = int(input())
+
+
+dq = deque()
+for _ in range(N):
+    cmd = input().split()
+    if cmd[0] == 'push':
+        dq.append(int(cmd[1]))
+        continue
+    if cmd[0] == 'pop':
+        if (len(dq) > 0):
+            print(dq.pop())
+        else:
+            print(-1)
+        continue
+    if cmd[0] == 'size':
+        print(len(dq))
+        continue
+    if cmd[0] == 'empty':
+        print(0 if len(dq) > 0 else 1)
+        continue
+    if cmd[0] == 'top':
+        len_d = len(dq)
+        if (len_d > 0):
+            print(dq[len_d-1])
+        else:
+            print(-1)
+        continue
