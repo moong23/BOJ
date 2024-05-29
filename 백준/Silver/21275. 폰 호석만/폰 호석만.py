604ad6bd047cd4c1dcd85b2100a8524e1322592e
Moon Ki, Kim <moonki0623@naver.com>
2024-05-29 10:52:51 +0900

    [Silver II] Title: 폰 호석만, Time: 40 ms, Memory: 31120 KB, Score: 24 point -BaekjoonHub

--- 백준/Silver/21275. 폰 호석만/폰 호석만.py
+++ 백준/Silver/21275. 폰 호석만/폰 호석만.py
@@ -1,6 +1,10 @@
 import sys
 input = sys.stdin.readline
 
+# import string
+# alphaIdx = list(map(str, range(10)))
+# alphaIdx.extend(list(string.ascii_lowercase))
+
 X_A, X_B = input().split()
 
 X, A, B = 0, 0, 0
@@ -12,10 +16,13 @@ for i in range(2, 37):
     except ValueError:
         continue
     for j in range(2, 37):
+        if i == j:
+            continue
         try:
             bTry = int(X_B, j)
             if aTry == bTry:
                 cnt += 1
+                # print(aTry, i, j)
                 if cnt >= 2:
                     print("Multiple")
                     sys.exit(0)
@@ -26,4 +33,5 @@ for i in range(2, 37):
             continue
 if cnt == 0:
     print("Impossible")
+    exit(0)
 print(X, A, B)
